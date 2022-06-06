5d41d8f58efeba1172955303e54a9cc982befbc4
ahmed-3bdo <tito.tito1874@gmail.com>
2022-06-06 20:11:15 +0200

    Edit and delete not neccessary js files

--- scripts/main.js
+++ scripts/main.js
@@ -4,8 +4,16 @@ $('.side-bar__btn-toggler').on('click', function() {
 // Theme Toggler
 $('.theme-toggler').on('click', function() {
     $(':root').toggleClass('light-theme');
-    $('.theme-toggler').toggleClass('theme-toggler-light')
-})
-
-
-
+    $('.theme-toggler').toggleClass('theme-toggler-light');
+    // Check if theme light && change help icon color
+    if ($(':root').hasClass('light-theme')) {
+      $('.help__icon img').attr('src', 'imgs/help-icon-dark.png')
+    } else {
+      $('.help__icon img').attr('src', 'imgs/help-icon-light.png')
+    }
+});
+// Keymap Content Toggler
+$('.keymap__icon').on('click', function() {
+  $(this).toggleClass('opened');
+  $('.keymap__content').slideToggle();
+});
